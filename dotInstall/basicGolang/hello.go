45d47d2aac7f2335e6db26414fb4a13ca7fd54f4
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2020-12-25 15:46:08 +0900

    定数

--- dotInstall/basicGolang/hello.go
+++ dotInstall/basicGolang/hello.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	number := 5
-	floatNumber := 3.14
-	message := "馬鹿な、ありえない"
-	flag := true
-	fmt.Printf("number:%d float:%f string:%s bool:%t", number, floatNumber, message, flag)
+	const message = "yuuです"
+	fmt.Println(message)
 }
