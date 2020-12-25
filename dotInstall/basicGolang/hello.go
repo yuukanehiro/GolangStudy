1b62cec9ca4b136c5f1525dd5c10a3dd4c7c3c68
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2020-12-25 15:38:40 +0900

    データ型

--- dotInstall/basicGolang/hello.go
+++ dotInstall/basicGolang/hello.go
@@ -3,18 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	massage := "hello world"
-	fmt.Println(massage)
-
-	number1, number2 := 11, 23
-	println(number1, number2)
-
-	var (
-		number3 int
-		message2 string
-	)
-	number3 = 35
-	message2 = "yahoo world"
+	number := 5
+	floatNumber := 3.14
+	message := "馬鹿な、ありえない"
+	flag := true
+	fmt.Printf("number:%d float:%f string:%s bool:%t", number, floatNumber, message, flag)
 }
-
-
