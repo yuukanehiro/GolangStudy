e9df691d58c3687ca9d5e5f3b26e7a0f8a65a1f6
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2020-12-25 18:49:12 +0900

    返り値のある関数

--- dotInstall/basicGolang/function.go
+++ dotInstall/basicGolang/function.go
@@ -6,7 +6,13 @@ func hello(name string) {
 	fmt.Println(name + "さんです")
 }
 
+func message() string {
+	message := "馬鹿な、ありえぬ。"
+	return message
+}
+
 func main() {
 	myName := "優"
 	hello(myName) // 優さんです
+	fmt.Println(message()) // 馬鹿な、ありえぬ。
 }
