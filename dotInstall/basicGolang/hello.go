c99bd3cb9b96d3ca64ca6fd87c0f2af7537bc2be
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2020-12-25 15:48:21 +0900

    iota

--- dotInstall/basicGolang/hello.go
+++ dotInstall/basicGolang/hello.go
@@ -4,5 +4,12 @@ import "fmt"
 
 func main() {
 	const message = "yuuです"
-	fmt.Println(message)
+	fmt.Println(message) // yuuです
+
+	const (
+		sun = iota
+		mon
+		tue
+	)
+	println(sun, mon, tue) // 0 1 2
 }
