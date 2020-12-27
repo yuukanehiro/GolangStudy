2b9b133e7ac903bb9ce91157bac99dc2231ed8bb
bravesoft-kanehiroyuu <kanehiro@yuulinux.tokyo>
2020-12-27 11:26:06 +0900

    短縮系でのスライス定義

---
+++ dotInstall/basicGolang/slice2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// make(), apped(), copy()
+	// slice1 := make([]int, 3) 
+	// fmt.Println(slice1) // [0 0 0]
+
+	// 短縮系でのスライス定義
+	slice2 := []int{1, 3, 5}
+	fmt.Println(slice2) // [1 3 5]
+}
