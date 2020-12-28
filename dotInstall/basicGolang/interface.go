b835bd951b6f1afdd83ba916a0a474602ddea4b5
bravesoft-kanehiroyuu <kanehiro@yuulinux.tokyo>
2020-12-28 23:51:27 +0900

    インターフェイス

---
+++ dotInstall/basicGolang/interface.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func show(t interface{}) {
+    //型アサーション
+    _, ok := t.(japanese)
+    if ok {
+        fmt.Println("i am japanese")
+    } else {
+        fmt.Println("i am not japanese")
+    }
+
+    // // 型Switch
+    // switch t.(type) {
+    // case japanese:
+    //     fmt.Println("i am japanese")
+    // default:
+    //     fmt.Println("i am not japanese")
+    // }
+}
+
+type japanese struct {}
+type american struct {}
+
+func (j japanese) greet() {
+	fmt.Println("おいっす！")
+}
+
+func (a american) greet() {
+	fmt.Println("Hello!")
+}
+
+type greeter interface {
+	greet()
+}
+
+func main() {
+	greeters := []greeter{japanese{}, american{}}
+	for _, greeter := range greeters {
+		greeter.greet()
+		show(greeter)
+	}
+}
+
+
+
+
+
+
+
